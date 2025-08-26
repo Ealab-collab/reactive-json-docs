170faa22ff91b33982885d48a4295540b736e543
Quang-Minh DANG <quang-minh@ea-lab.io>
2025-08-26 15:35:32 +0200

    v1: reactive-json-bootstrap integrated; reactive-json v1.

--- src/hull/CustomReactiveJsonRoot.jsx
+++ src/hull/CustomReactiveJsonRoot.jsx
@@ -1,12 +1,13 @@
 import { ReactiveJsonRoot } from "@ea-lab/reactive-json";
 import { mergeComponentCollections } from "@ea-lab/reactive-json";
+import { bootstrapComponentsPlugin } from "@ea-lab/reactive-json-bootstrap";
 import { chartjsComponents } from "@ea-lab/reactive-json-chartjs";
 import { demoPlugins } from "./component-demo";
 
 export const CustomReactiveJsonRoot = (props) => {
     const additionalProps = {};
 
-    additionalProps.plugins = mergeComponentCollections([chartjsComponents, demoPlugins]);
+    additionalProps.plugins = mergeComponentCollections([bootstrapComponentsPlugin, chartjsComponents, demoPlugins]);
 
     const finalProps = { ...props, ...additionalProps };
 
